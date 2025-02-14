9b5c18ff659bd36b3f919c9b433f66711fdce509
wjwj910 <wjwj910@gmail.com>
2025-02-14 12:48:07 +0900

    3단계

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,6 +16,9 @@ fun main() {
 
             print("작가) ")
             val author = readlnOrNull()!!.trim()
+
+            val id = 1
+            println("${id}번 명언이 등록되었습니다.")
         }
     }
 }
