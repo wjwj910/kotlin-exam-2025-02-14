526d730f664534982f0dad63cdf94aaee416321e
wjwj910 <wjwj910@gmail.com>
2025-02-14 15:33:25 +0900

    8단계

--- src/main/kotlin/domain/wiseSaying/wiseSaying/controller/WiseSayingController.kt
+++ src/main/kotlin/domain/wiseSaying/wiseSaying/controller/WiseSayingController.kt
@@ -54,4 +54,32 @@ class WiseSayingController {
 
         println("${id}번 명언을 삭제하였습니다.")
     }
+
+    fun actionModify(rq: Rq) {
+        val id = rq.getParamValueAsInt("id", 0)
+
+        if (id == 0) {
+            println("id를 정확히 입력해주세요.")
+            return
+        }
+
+        val wiseSaying = wiseSayings.firstOrNull { it.id == id }
+
+        if (wiseSaying == null) {
+            println("${id}번 명언은 존재하지 않습니다.")
+            return
+        }
+
+        println("명언(기존) : ${wiseSaying.content}")
+        print("명언 : ")
+        val content = readlnOrNull()!!.trim()
+
+        println("작가(기존) : ${wiseSaying.author}")
+        print("작가 : ")
+        val author = readlnOrNull()!!.trim()
+
+        wiseSaying.update(content, author)
+
+        println("${id}번 명언을 수정하였습니다.")
+    }
 }
--- src/main/kotlin/domain/wiseSaying/wiseSaying/entity/WiseSaying.kt
+++ src/main/kotlin/domain/wiseSaying/wiseSaying/entity/WiseSaying.kt
@@ -4,4 +4,9 @@ data class WiseSaying (
     val id: Int,
     var content: String,
     var author: String
-)
+) {
+    fun update(content: String, author: String) {
+        this.content = content
+        this.author = author
+    }
+}
