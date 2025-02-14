e48cb52ca21e24ecb172892c2e12421ee1c877d6
wjwj910 <wjwj910@gmail.com>
2025-02-14 16:14:13 +0900

    패키지 리팩토링(리포지토리 도입)

--- src/main/kotlin/domain/wiseSaying/wiseSaying/service/WiseSayingService.kt
+++ src/main/kotlin/domain/wiseSaying/wiseSaying/service/WiseSayingService.kt
@@ -1,36 +1,34 @@
 package com.domain.wiseSaying.wiseSaying.service
 
 import com.domain.wiseSaying.wiseSaying.entity.WiseSaying
+import com.domain.wiseSaying.wiseSaying.repository.WiseSayingRepository
 
 class WiseSayingService {
-    private var lastId = 0
-    private val wiseSayings = mutableListOf<WiseSaying>()
+    private val wiseSayingRepository = WiseSayingRepository()
 
     fun write(content: String, author: String): WiseSaying {
-        val id = ++lastId
-
-        return WiseSaying(id, content, author).apply {
-            wiseSayings.add(this)
-        }
+        return wiseSayingRepository.save(WiseSaying(content, author))
     }
 
     fun isEmpty(): Boolean {
-        return wiseSayings.isEmpty()
+        return wiseSayingRepository.isEmpty()
     }
 
     fun findAll(): List<WiseSaying> {
-        return wiseSayings
+        return wiseSayingRepository.findAll()
     }
 
     fun findById(id: Int): WiseSaying? {
-        return wiseSayings.find { it.id == id }
+        return wiseSayingRepository.findById(id)
     }
 
     fun delete(wiseSaying: WiseSaying) {
-        wiseSayings.remove(wiseSaying)
+        wiseSayingRepository.delete(wiseSaying)
     }
 
     fun modify(wiseSaying: WiseSaying, content: String, author: String) {
         wiseSaying.modify(content, author)
+
+        wiseSayingRepository.save(wiseSaying)
     }
 }
