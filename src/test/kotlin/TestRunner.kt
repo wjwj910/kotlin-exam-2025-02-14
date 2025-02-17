48833bf91edb29b4a58464a2e53f83d4f7dcb48b
wjwj910 <wjwj910@gmail.com>
2025-02-17 09:35:13 +0900

    스타일 변경 및 주석 추가

--- src/test/kotlin/TestRunner.kt
+++ src/test/kotlin/TestRunner.kt
@@ -10,16 +10,29 @@ object TestRunner {
     private val originalOut: PrintStream = System.out
 
     fun run(input: String): String {
+        // 표준 입력 리다이렉팅
+        // [키보드 입력 => 문자열 입력] 변환
         val outputStream = ByteArrayOutputStream()
         val printStream = PrintStream(outputStream)
 
-        System.setIn(ByteArrayInputStream(("$input\n종료").trim().toByteArray()))
+        System.setIn(
+            ByteArrayInputStream(
+                ("$input\n종료")
+                    .trim()
+                    .toByteArray()
+            )
+        )
+        // 표준 출력 리다이렉팅
+        // [화면 출력 => 문자열 출력] 변환
         System.setOut(printStream)
 
         App().run()
 
+        // 표준 출력 결과를 문자열로 변환
+        // 윈도우와 Mac OS 개행 문자 차이로 인한 표준화
         val result = outputStream.toString().trim().replace(Regex("\\r\\n"), "\n")
 
+        // 다시 표준 입출력으로 북구
         System.setIn(originalIn)
         System.setOut(originalOut)
 
