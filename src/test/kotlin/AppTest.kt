e237a0c900c5b0f16f9a30ae9adc61bbbde36005
wjwj910 <wjwj910@gmail.com>
2025-02-17 10:28:32 +0900

    23강, 명언 삭제 테스트

--- src/test/kotlin/AppTest.kt
+++ src/test/kotlin/AppTest.kt
@@ -34,4 +34,23 @@ class AppTest {
         assertThat(result).contains("1 / 충무공 이순신 / 나의 죽음을 적들에게 알리지 말라.")
         assertThat(result).contains("2 / 에디슨 / 천재는 99%의 노력과 1%의 영감이다.")
     }
+
+    @Test
+    fun `명언 삭제`() {
+        val result = TestRunner.run(
+            """
+            등록
+            나의 죽음을 적들에게 알리지 말라.
+            충무공 이순신
+            등록
+            천재는 99%의 노력과 1%의 영감이다.
+            에디슨
+            삭제?id=1
+            목록
+        """
+        )
+        assertThat(result).contains("1번 명언을 삭제하였습니다.")
+        assertThat(result).doesNotContain("1 / 충무공 이순신 / 나의 죽음을 적들에게 알리지 말라.")
+        assertThat(result).contains("2 / 에디슨 / 천재는 99%의 노력과 1%의 영감이다.")
+    }
 }
