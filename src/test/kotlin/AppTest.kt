1d4d64d9f5ef4090d2dee21b7fc3ba4971a03fda
wjwj910 <wjwj910@gmail.com>
2025-02-17 10:22:54 +0900

    명언 목록 테스트
    * fun 에 ` `을 넣어서, DisplayName 쓰지 않고 사용.
    * 함수 명 자체를 이름으로 지정할 수 있다.

--- src/test/kotlin/AppTest.kt
+++ src/test/kotlin/AppTest.kt
@@ -17,4 +17,21 @@ class AppTest {
         assertContains(result, "작가 : ")
         assertContains(result, "1번 명언이 등록되었습니다.")
     }
+
+    @Test
+    fun `명언 목록`() {
+        val result = TestRunner.run(
+            """
+            등록
+            나의 죽음을 적들에게 알리지 말라.
+            충무공 이순신
+            등록
+            천재는 99%의 노력과 1%의 영감이다.
+            에디슨
+            목록
+        """
+        )
+        assertContains(result, "1 / 충무공 이순신 / 나의 죽음을 적들에게 알리지 말라.")
+        assertContains(result, "2 / 에디슨 / 천재는 99%의 노력과 1%의 영감이다.")
+    }
 }
