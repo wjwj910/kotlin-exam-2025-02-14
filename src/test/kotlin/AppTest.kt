f28ca382b9a6345996b050868956bebab724e484
wjwj910 <wjwj910@gmail.com>
2025-02-17 10:30:53 +0900

    24강, 명언 수정 테스트

--- src/test/kotlin/AppTest.kt
+++ src/test/kotlin/AppTest.kt
@@ -53,4 +53,22 @@ class AppTest {
         assertThat(result).doesNotContain("1 / 충무공 이순신 / 나의 죽음을 적들에게 알리지 말라.")
         assertThat(result).contains("2 / 에디슨 / 천재는 99%의 노력과 1%의 영감이다.")
     }
+
+    @Test
+    fun `명언 수정`() {
+        val result = TestRunner.run(
+            """
+            등록
+            나의 죽음을 적들에게 알리지 말라.
+            충무공 이순신
+            수정?id=1
+            나의 죽음을 적들에게 알리지 말라. 그리고 적들에게 나의 삶을 알리라.
+            이순신 장군
+            목록
+        """
+        )
+        assertThat(result).contains("1번 명언을 수정하였습니다.")
+        assertThat(result).doesNotContain("1 / 충무공 이순신 / 나의 죽음을 적들에게 알리지 말라.")
+        assertThat(result).contains("1 / 이순신 장군 / 나의 죽음을 적들에게 알리지 말라. 그리고 적들에게 나의 삶을 알리라.")
+    }
 }
